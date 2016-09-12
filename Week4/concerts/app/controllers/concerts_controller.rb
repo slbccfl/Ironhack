66889a9de6ac9dcf7d81dcfbbef700a6f862b165
Steven Barnes <slbccfl@gmail.com>
2016-09-12 14:25:43 -0400

    Got date selections working for concert project

--- Week4/concerts/app/controllers/concerts_controller.rb
+++ Week4/concerts/app/controllers/concerts_controller.rb
@@ -3,18 +3,25 @@ class ConcertsController < ApplicationController
 	end
 
 	def create
+		# render plain: params[:article].inspect
 		@concert = Concert.new(
-		  	:artist => params[:artist],
-		  	:venue => params[:venue],
-			:city => params[:city],
-			:datetime => params[:datetime],
-			:price => params[:price],
-			:description => params[:description])
+		  	:artist => params[:concert][:artist],
+		  	:venue => params[:concert][:venue],
+			:city => params[:concert][:city],
+			:datetime => params[:concert][:datetime],
+			:price => params[:concert][:price],
+			:description => params[:concert][:description])
 	 
 		@concert.save
 		redirect_to @concert
 	end
 
+	def index
+		@todays_concerts = Concert.where(datetime: Time.now.beginning_of_day..Time.now.end_of_day)
+		@upcoming_concerts = Concert.where(datetime: Time.now.tomorrow.beginning_of_day..Time.now.days_since(30).end_of_day)
+	end
+
 	def show
+		@concert = Concert.find(params[:id])
 	end
 end
