2e637c58dccb238c021fb1e34151db69720a1298
Steven Barnes <slbccfl@gmail.com>
2016-09-15 17:45:00 -0400

    spotify project through iteration 2

--- Week5/spotify/app.js
+++ Week5/spotify/app.js
@@ -4,27 +4,58 @@ $(document).ready(function() {
 	$('.js-artist-search').on("click", artistSearch);
 })
 
-function artistSearch() {
-	console.log('artistSearch');
+function artistSearch(theSearch) {
+	theSearch.preventDefault();
 	q = 'q=artist:' + $(".js-artist-name").val() + '&type=artist';
-	console.log(q);
 	$.ajax({
 		type: "GET",
 		url: `https://api.spotify.com/v1/search?${q}`,
 		success: showArtists,
 		error: handleError
 	});
-}
+};
 
 function showArtists(response) {
-	console.log("Successful GET");
-	console.log(response);
-	artistHash = JSON.parse(response);
-	
-
-}
+	artistsArray = response.artists.items;
+	htmlArtistList = '';
+	artistsArray.forEach(function(artist) {
+		htmlArtistList += `<li><h2>${artist.name}</h2>`
+		htmlArtistList += `<button class="js-album-search" data-artist-id=${artist.id}>See artist's albums</button>`
+		if (artist.images.length > 0) {
+			htmlArtistList += `<br><img src=${artist.images[0].url} width=200></li>`;
+		}
+		
+	});
+	$('.artistList').html(htmlArtistList);
+	$(".js-album-search").on("click",albumSearch)
+};
 
 function handleError(error) {
 	console.log("Error!");
 	console.log(error.responseText);
-}
+};
+
+ 
+
+function albumSearch (theEvent) {
+	artistId = $(theEvent.currentTarget).data("artist-id");
+
+	console.log("Album Search CLICK!! on artist.id: " + artistId);
+		$.ajax({
+		type: "GET",
+		url: `https://api.spotify.com/v1/artists/${artistId}/albums`,
+		success: showAlbums,
+		error: handleError
+	});
+};
+
+function showAlbums(response) {
+	albumArray = response.items;
+	var albumHTML = '';
+	albumArray.forEach(function(album) {
+		albumHTML += `<li>${album.name}</li>` ;
+	});
+	$('.js-album-list').html(albumHTML);
+	console.log(albumHTML);
+	$(".js-album-modal").modal("show");
+};
